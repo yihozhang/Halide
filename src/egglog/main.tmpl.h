a96492c0f0a2c4b1499aeb114153609e9046dbcc
Yihong Zhang <yz489@cs.washington.edu>
2024-10-14 17:30:39 -0700

    checkpoint

--- src/egglog/main.tmpl.h
+++ src/egglog/main.tmpl.h
@@ -5,6 +5,7 @@ auto EGGLOG_PROG = [](std::vector<std::pair<std::string, std::string>>&& binding
 #include "analysis/typechecking.egg"
 #include "optimization/axiom.egg"
 #include "optimization/constant_folding.egg"
+#include "optimization/index_tweaking.egg"
 #include "optimization/amx.egg"
     for (auto [name, src] : bindings) {
         prog += "(let " + name + " " + src + ")\n";
